7d46431cff84963c4a3df324577c5096442a6984
ssulham <sean.sulham@hotmail.com>
2026-01-02 16:27:29 -0500

    Added to GUI.

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ label = sg.Text('Label')
 input_box = sg.InputText(tooltip="Input Text")
 add_btn = sg.Button('Add')
 
-window = sg.Window("My To-Do List", layout=[[label], [input_box, add_btn]])
+window = sg.Window("My To-Do List", layout=[[label], [input_box], [add_btn]])
 window.read()
 window.close()
