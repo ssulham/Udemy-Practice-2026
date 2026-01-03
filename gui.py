d97fef7f668aceac272437198faf5a0cf37b4234
ssulham <sean.sulham@hotmail.com>
2026-01-03 15:58:44 -0500

    Implemented Edit and Complete Buttons to GUI.

--- gui.py
+++ gui.py
@@ -1,10 +1,14 @@
 import file_access
 import FreeSimpleGUI as sg
 
-from file_access import give_todos
-
 label = sg.Text('Label')
 input_box = sg.InputText(tooltip="Input Text", key='todo')
+list_box = sg.Listbox(
+    values=file_access.get_todos(),
+    key='todos',
+    enable_events=True,
+    size=(45, 10)
+)
 add_btn = sg.Button('Add')
 edit_btn = sg.Button('Edit')
 complete_btn = sg.Button('Complete')
@@ -12,12 +16,15 @@ complete_btn = sg.Button('Complete')
 title = "My To-Do App"
 layout = [
     [label],
-    [input_box],
-    [add_btn, edit_btn, complete_btn]
+    [input_box, add_btn],
+    [list_box],
+    [edit_btn, complete_btn]
 ]
 font = ("Helvetica", 20)
 
 window = sg.Window(title, layout, font=font)
+
+# Code Execution
 
 while True:
     event, values = window.read()
@@ -29,10 +36,22 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             file_access.give_todos(todos)
+            window['todos'].update(todos)
         case 'Edit':
-            break
+            old_todo = values["todos"][0]
+            new_todo = values["todo"] + "\n"
+            todos = file_access.get_todos()
+            index = todos.index(old_todo)
+            todos[index] = new_todo
+            file_access.give_todos(todos)
+            window["todos"].update(todos)
         case 'Complete':
-            break
+            old_todo = values["todos"][0]
+            todos = file_access.get_todos()
+            index = todos.index(old_todo)
+            todos.pop(index)
+            file_access.give_todos(todos)
+            window['todos'].update(todos)
         case sg.WIN_CLOSED:
             break
 print("Program properly closed.")
